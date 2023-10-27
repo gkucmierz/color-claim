04ecb105a06c2e55cf7f10c0c6b3a9de011287a0
Grzegorz Kućmierz <gkucmierz@gmail.com>
2023-10-27 17:17:19 +0200

    add fields

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,30 @@
 <script setup>
 import { ref } from 'vue'
 
-defineProps({
-  msg: String,
-})
+import InputText from 'primevue/inputtext';
+
+let colorName = ref('');
+let btcAddr = ref('')
+
+
 
 </script>
 
 <template>
   <h1>Color Claim</h1>
 
-  <p>
+  <div>
     Choose, name and claim your own color.
-  </p>
+  </div>
+
+  <div>
+    <InputText type="text" v-model="colorName" />
+    <InputText type="text" v-model="btcAddr" />
+  </div>
+
+{{ colorName }}
+{{ btcAddr }}
+
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
+
+import PrimeVue from 'primevue/config';
+import Button from "primevue/button";
+
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(PrimeVue);
+app.component('Button', Button);
+
+app.mount('#app');
