428832c05a40ae72a45b17591b44bd6d70c5de98
Grzegorz Kućmierz <gkucmierz@gmail.com>
2023-10-27 16:11:59 +0200

    init project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+</script>
+
+<template>
+  <h1>Color Claim</h1>
+
+  <p>
+    Choose, name and claim your own color.
+  </p>
+</template>
+
+<style scoped>
+</style>
