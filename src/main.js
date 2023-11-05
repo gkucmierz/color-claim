2090057b9d13e1c1ba9be1871a16762287cabccb
Grzegorz Kućmierz <gkucmierz@gmail.com>
2023-11-05 19:13:49 +0100

    cleanup

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router';
 import PrimeVue from 'primevue/config';
 import Button from "primevue/button";
 import Tooltip from 'primevue/tooltip';
+import InputText from 'primevue/inputtext';
 
 import './style.css'
 import App from './App.vue'
@@ -14,5 +15,6 @@ app.use(router);
 app.use(PrimeVue, { ripple: true });
 app.directive('tooltip', Tooltip);
 app.component('Button', Button);
+app.component('InputText', InputText);
 
 app.mount('#app');
--- src/views/HelloWorld.vue
+++ src/views/HelloWorld.vue
@@ -2,25 +2,11 @@
 import { ref } from 'vue';
 
 import ColorCard from '../components/ColorCard.vue';
-
-import InputText from 'primevue/inputtext';
 import ColorPicker from 'primevue/colorpicker';
-import Button from 'primevue/button';
-// import Dialog from 'primevue/dialog';
-
-
-import { QrcodeStream } from 'vue-qrcode-reader';
-
 
 const color = ref('');
 const colorName = ref('');
 const btcAddr = ref('');
-const qrDialog = ref(false);
-
-const onDetect = (scans) => {
-  btcAddr.value = (scans[0].rawValue || '').replace(/bitcoin\:/, '');
-  qrDialog.value = false;
-};
 
 </script>
 
@@ -35,18 +21,11 @@ const onDetect = (scans) => {
       <form @submit="onSubmit" class="flex flex-column gap-2">
         <ColorCard :color="color" :name="colorName"></ColorCard>
         <InputText type="text" v-model="colorName" placeholder="Color Name" />
-        <div class="p-inputgroup flex-1">
-          <InputText type="text" v-model="btcAddr" placeholder="BTC Address" v-tooltip.top="'Please use only Ordinals compatible wallets like UniSats, Xverse or Leather. Otherwise you might have a problem do access you inscription.'" label="Button" />
-          <!-- <Button icon="pi pi-qrcode" severity="secondary" @click="qrDialog = true" /> -->
-        </div>
+        <InputText type="text" v-model="btcAddr" placeholder="BTC Address" v-tooltip.top="'Please use only Ordinals compatible wallets like UniSats, Xverse or Leather. Otherwise you might have a problem do access you inscription.'" label="Button" />
         <Button label="Claim" />
       </form>
     </div>
   </div>
-
-<!--   <Dialog v-model:visible="qrDialog" modal header="Scan Bitcoin Address">
-    <qrcode-stream @detect="onDetect"></qrcode-stream>
-  </Dialog> -->
 
 </template>
 
