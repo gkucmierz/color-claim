e29796082e5f793c696bdb155ef07fc01c9eb51e
Grzegorz Kućmierz <gkucmierz@gmail.com>
2023-11-05 18:50:53 +0100

    move preview and color picker

--- src/views/HelloWorld.vue
+++ src/views/HelloWorld.vue
@@ -27,11 +27,12 @@ const onDetect = (scans) => {
   <div class="flex">
     <div class="">
       <div>Choose, name and claim your own color.</div>
+      <ColorPicker v-model="color" inline />
     </div>
 
     <div class="flex justify-content-center flex-column flex-wrap">
       <form @submit="onSubmit" class="flex flex-column gap-2">
-        <ColorPicker v-model="color" inline />
+        <ColorCard :color="color" :name="colorName"></ColorCard>
         <InputText type="text" v-model="colorName" placeholder="Color Name" />
         <div class="p-inputgroup flex-1">
           <InputText type="text" v-model="btcAddr" placeholder="BTC Address" />
@@ -42,14 +43,9 @@ const onDetect = (scans) => {
     </div>
   </div>
 
-  <div>
-    <p>Preview</p>
-    <ColorCard :color="color" :name="colorName"></ColorCard>
-  </div>
-
-  <Dialog v-model:visible="qrDialog" modal header="Scan Bitcoin Address">
+<!--   <Dialog v-model:visible="qrDialog" modal header="Scan Bitcoin Address">
     <qrcode-stream @detect="onDetect"></qrcode-stream>
-  </Dialog>
+  </Dialog> -->
 
 </template>
 
