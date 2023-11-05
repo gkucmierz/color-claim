8a63203cc30e51952923342ad57ff1ac11f1e6b9
Grzegorz Kućmierz <gkucmierz@gmail.com>
2023-11-06 00:02:57 +0100

    place inputs horizontally

--- src/views/HelloWorld.vue
+++ src/views/HelloWorld.vue
@@ -25,8 +25,10 @@ const getRGB = () => convert.hex.rgb(color.value).join(' ');
     <div class="flex justify-content-center flex-column flex-wrap">
       <form @submit="onSubmit" class="flex flex-column gap-2">
         <ColorCard :color="color" :name="colorName"></ColorCard>
-        <InputText type="text" :value="getRGB()" v-tooltip="'RGB'" readonly/>
-        <InputText type="text" :value="getHEX()" v-tooltip="'HEX'" readonly/>
+        <div class="flex flex-row gap-2">
+          <InputText type="text" :value="getRGB()" v-tooltip="'RGB'" readonly/>
+          <InputText type="text" :value="getHEX()" v-tooltip="'HEX'" readonly/>
+        </div>
         <InputText type="text" v-model="colorName" placeholder="Color Name" v-tooltip="'Color Name'" />
         <InputText type="text" v-model="btcAddr" placeholder="BTC Address" v-tooltip="'Please use only Ordinals compatible wallets like UniSats, Xverse or Leather. Otherwise you might have a problem to access your inscription.'" label="Button" />
         <Button label="Claim" />
