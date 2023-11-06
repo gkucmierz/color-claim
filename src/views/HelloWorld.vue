090988e88ddc1ecc31701226f63dc0e2172f0174
Grzegorz Kućmierz <gkucmierz@gmail.com>
2023-11-06 21:58:40 +0100

    editable hex value

--- src/views/HelloWorld.vue
+++ src/views/HelloWorld.vue
@@ -6,12 +6,24 @@ import ColorPicker from 'primevue/colorpicker';
 
 import convert from 'color-convert';
 
+const randomColor = () => {
+  return convert.rgb.hex([256, 256, 256].map(n => Math.floor(Math.random()*n)));
+};
+
 const color = ref('');
+const hashColor = ref('');
 const colorName = ref('');
 const btcAddr = ref('');
 
 const getHEX = () => `#${color.value}`;
 const getRGB = () => convert.hex.rgb(color.value).join(' ');
+
+watch(hashColor, () => {
+  color.value = hashColor.value.trim().replace(/#/, '');
+});
+watch(color, () => hashColor.value = `#${color.value}`);
+
+color.value = randomColor();
 
 </script>
 
@@ -27,7 +39,7 @@ const getRGB = () => convert.hex.rgb(color.value).join(' ');
         <ColorCard :color="color" :name="colorName"></ColorCard>
         <div class="flex flex-row gap-2">
           <InputText type="text" :value="getRGB()" v-tooltip="'RGB'" readonly/>
-          <InputText type="text" :value="getHEX()" v-tooltip="'HEX'" readonly/>
+          <InputText type="text" v-model="hashColor" v-tooltip="'HEX'"/>
         </div>
         <InputText type="text" v-model="colorName" placeholder="Color Name" v-tooltip="'Color Name'" />
         <InputText type="text" v-model="btcAddr" placeholder="BTC Address" v-tooltip="'Please use only Ordinals compatible wallets like UniSats, Xverse or Leather. Otherwise you might have a problem to access your inscription.'" label="Button" />
