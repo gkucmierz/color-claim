eab98419a033cd05c26d1d89755184d9a485de48
Grzegorz Kućmierz <gkucmierz@gmail.com>
2023-11-08 15:28:39 +0100

    move storage to utils; add btcAddr in status

--- src/views/Status.vue
+++ src/views/Status.vue
@@ -4,11 +4,6 @@ import { ref } from 'vue';
 import { generate } from '../utils/certificate';
 import { STORAGE_KEY_COLOR, STORAGE_KEY_NAME, STORAGE_KEY_BTC_ADDR, readStorage } from '../utils/storage';
 
-const readStorage = (key, def = '') => {
-  const item = localStorage.getItem(key);
-  if (item === null) return def;
-  return item;
-};
 
 const color = ref(readStorage(STORAGE_KEY_COLOR));
 const colorName = ref(readStorage(STORAGE_KEY_NAME));
