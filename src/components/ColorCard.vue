4e9cbde955ea688a34b990968ddf3815bf2a27f8
Grzegorz Kućmierz <gkucmierz@gmail.com>
2023-11-12 22:15:34 +0100

    add xss protection for svg

--- src/components/ColorCard.vue
+++ src/components/ColorCard.vue
@@ -2,6 +2,7 @@
 import { ref, watch } from 'vue';
 import { encode } from 'js-base64';
 import fontColorContrast from 'font-color-contrast';
+import xss from 'xss';
 
 const px = n => `${n}px`;
 
@@ -33,9 +34,9 @@ const naturalSearch = (cond, retFirstTrue = true) => {
 const getSvgBody = (color, name, size) => {
   return `
     <svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">
-      <rect width="100%" height="100%" fill="#${color}"/>
+      <rect width="100%" height="100%" fill="#${xss(color)}"/>
 
-      <text x="50%" y="50%" font-family="Arial" font-size="${Math.round(size)}%" fill="${fontColorContrast('#'+color)}" text-anchor="middle" dominant-baseline="middle">${name}</text>
+      <text x="50%" y="50%" font-family="Arial" font-size="${Math.round(size)}%" fill="${fontColorContrast('#'+color)}" text-anchor="middle" dominant-baseline="middle">${xss(name)}</text>
     </svg>
   `;
 };
