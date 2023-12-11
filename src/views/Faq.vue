81bb1e1530f82c4a12fa62050e4e913436e2c5e6
Szczepan Bentyn <szczepan@personaltokens.io>
2023-12-11 14:36:34 -0300

    Corrected responsivness, button full size, new generated bg

--- src/views/Faq.vue
+++ src/views/Faq.vue
@@ -4,11 +4,41 @@ import { ref } from 'vue';
 </script>
 
 <template>
-  <h1>Frequently Asked Questions</h1>
+<h1>Frequently Asked Questions</h1>
 
-  <h3>Lorem ipsum</h3>
-  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-  
+<h3>What is Color Claim project about?</h3>
+<p>Thanks to Bitcoin and Ordinals technology, first time in history are are able to build a global, open and immutable directory of color names. We think that anyone who wants, should be able to name a color.</p>
+
+<h3>Who gave you permission to do that?</h3>
+<p>No one.</p>
+
+<h3>Is the Color Claim inscription and/or certificate a legally binding claim on the color?</h3>
+<p>Let's leave it to lawyers and judges to decide.</p>
+
+<h3>What's the best method for payment, Bitcoin onchain or Lightning Network?</h3>
+<p>To ensure faster and free transactions, we advise utilizing the Lightning Network for your inscriptions. Transactions with Bitcoin onchain might take longer due to the necessity of 1-confirmation.</p>
+
+<h3>Is it viable to make payments through a cryptocurrency exchange?</h3>
+<p>Certainly, payments from exchanges are accepted. Keep in mind the fees exchanges apply. Your payment should include these fees to match the exact amount specified in the invoice.</p>
+
+<h3>Could you explain your service fees in detail?</h3>
+<p>Inscribing ordinals involves considerable costs. We recommend thorough research before using our service. The cost is file size dependent, with our service charge being 5 USD in BTC. Most of the expense is from Bitcoin Network fees.</p>
+
+<h3>Which wallets are suitable for making payments?</h3>
+<p>Any BTC or Lightning wallet can be used for payments. The wallet used for payment does not have to be the one where your inscription is sent.</p>
+
+<h3>Is there a way to modify the fee rate after initiating a payment?</h3>
+<p>Adjusting the fee rate post-payment isn't possible. It's important to be comfortable with your chosen fee rate from the start. Lower rates may lead to longer confirmation times, occasionally more than a day.</p>
+
+<h3>How should I proceed if my payment is insufficient?</h3>
+<p>If your payment is less than the required amount due to factors like exchange or miner fees, your order will be marked as underpaid. You can then complete your payment or request a refund.</p>
+
+<h3>Is it possible to receive multiple ordinals at a single address?</h3>
+<p>Yes, receiving several ordinals at one address is feasible. However, it's important to manage your coins carefully to ensure the safety of your ordinals during transfers. We also advice to use only Ordinals compatible wallets like UniSats, Xverse or Leather to ensure full control over your inscriptions.</p>
+
+<h3>After making a payment, how do I track my inscription?</h3>
+<p>Following the confirmation of your payment, your Ordinal will be inscribed to an internal wallet, and your order will move to the delivery phase. You can monitor the delivery status and set a receiving address, if not already done, via the order checker.</p>
+
 </template>
 
 <style scoped>
